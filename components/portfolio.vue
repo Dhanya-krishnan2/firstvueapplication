83e6a61474f97771e6d2a35a2e05386b9dac6962
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2021-03-10 13:20:36 +0800

    added vuetify in the front end

---
+++ components/portfolio.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <div id="profile-container">
+      <img src="~/assets/img/dhanya3prifile.jpg" alt="my picture" />
+      <h1>{{name}}</h1>
+
+     <h3> {{description}}</h3>
+    </div>
+
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Dhanya Kunhikrishnnan",
+      description: "I am a full stack developer from Australia"
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+<portfolio />
+
+</template>
+
+<script>
+ import Portfolio from "~/components/portfolio";
+// import Portfolio from '../components/portfolio.vue';
+export default {
+  components:{
+    Portfolio
+
+  }
+
+}
+</script>
+
+<style>
+
+</style>
